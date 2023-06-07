98317d23416a75646acb1cf2b73da3540787eccb
wrich-crypto <416116073@qq.com>
2023-06-07 17:38:51 +0800

    gpt

--- modules/admin/middleware/middleware.go
+++ modules/admin/middleware/middleware.go
@@ -10,7 +10,15 @@ func init() {
 	if config.Config.Middleware.Authority.Enable {
 		g.Server().BindMiddleware("/admin/api_key/*", AdminTokenAuthMiddleware)
 		g.Server().BindMiddleware("/admin/dev_config/*", AdminTokenAuthMiddleware)
-		g.Server().BindMiddleware("/admin/agent/*", TokenAuthMiddleware)
+		g.Server().BindMiddleware("/admin/agent/add", AdminTokenAuthMiddleware)
+		g.Server().BindMiddleware("/admin/agent/delete", AdminTokenAuthMiddleware)
+		g.Server().BindMiddleware("/admin/agent/update", AdminTokenAuthMiddleware)
+		g.Server().BindMiddleware("/admin/agent/info", AdminTokenAuthMiddleware)
+		g.Server().BindMiddleware("/admin/agent/list", AdminTokenAuthMiddleware)
+		g.Server().BindMiddleware("/admin/agent/page", AdminTokenAuthMiddleware)
+		g.Server().BindMiddleware("/admin/agent/add_balance", AdminTokenAuthMiddleware)
+		g.Server().BindMiddleware("/admin/agent/update_agent", TokenAuthMiddleware)
+		g.Server().BindMiddleware("/admin/agent/get_agent", TokenAuthMiddleware)
 	}
 	if config.Config.Middleware.Log.Enable {
 		g.Server().BindMiddleware("/admin/*", BaseLog)
