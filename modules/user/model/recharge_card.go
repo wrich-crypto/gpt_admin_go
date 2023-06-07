5b0420066d5cb58093c29ac23e04b5bf2ec25710
wrich-crypto <416116073@qq.com>
2023-06-07 16:31:28 +0800

    gpt

--- modules/user/model/recharge_card.go
+++ modules/user/model/recharge_card.go
@@ -11,7 +11,7 @@ type RechargeCards struct {
 	CardAccount    string    `gorm:"column:card_account;size:20;unique;not null" json:"card_account"`
 	CardPassword   string    `gorm:"column:card_password;size:20;not null" json:"card_password"`
 	ExpireTime     time.Time `gorm:"column:expire_time" json:"expire_time"`
-	RechargeAmount float64   `gorm:"column:recharge_amount;type:decimal(20,2);not null"`
+	RechargeAmount float64   `gorm:"column:recharge_amount;type:decimal(20,2);not null" json:"recharge_amount"`
 	UsedPoints     int       `gorm:"column:used_points" json:"used_points"`
 	BoundUser      string    `gorm:"column:bound_user;size:100" json:"bound_user"`
 	CreateUser     string    `gorm:"column:create_user;size:100" json:"create_user"`
