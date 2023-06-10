90f00945637e4f1b420e05c7c5377aee90a8832c
wrich-crypto <416116073@qq.com>
2023-06-10 21:51:46 +0800

    gpt

---
+++ modules/user/model/user_order.go
@@ -0,0 +1,35 @@
+package model
+
+import (
+	"github.com/cool-team-official/cool-admin-go/cool"
+)
+
+type UserOrders struct {
+	*cool.Model
+	TradeNo   string  `gorm:"column:trade_no;type:varchar(100);not null"`
+	UserID    int     `gorm:"column:user_id;not null"`
+	OrderType int     `gorm:"column:order_type;not null;default:1"`
+	Amount    float64 `gorm:"column:amount;type:decimal(10,2);not null"`
+	Status    int     `gorm:"column:status;not null;default:1"`
+}
+
+const TableNameUserOrders = "user_orders"
+
+func (*UserOrders) TableName() string {
+	return TableNameUserOrders
+}
+
+func (*UserOrders) GroupName() string {
+	return "default"
+}
+
+func NewUserOrders() *UserOrders {
+	return &UserOrders{
+		Model: cool.NewModel(),
+	}
+}
+
+// init 创建表
+func init() {
+	cool.CreateTable(&UserOrders{})
+}
