9c82b59702e841519d0bb8e72f35272250530794
wrich-crypto <416116073@qq.com>
2023-06-10 22:22:21 +0800

    gpt

--- modules/user/model/user_order.go
+++ modules/user/model/user_order.go
@@ -1,16 +1,19 @@
 package model
 
 import (
+	"time"
+
 	"github.com/cool-team-official/cool-admin-go/cool"
 )
 
 type UserOrders struct {
 	*cool.Model
-	TradeNo   string  `gorm:"column:trade_no;type:varchar(100);not null"`
-	UserID    int     `gorm:"column:user_id;not null"`
-	OrderType int     `gorm:"column:order_type;not null;default:1"`
-	Amount    float64 `gorm:"column:amount;type:decimal(10,2);not null"`
-	Status    int     `gorm:"column:status;not null;default:1"`
+	TradeNo   string    `gorm:"column:trade_no;type:varchar(100);not null"`
+	UserID    int       `gorm:"column:user_id;not null"`
+	OrderType int       `gorm:"column:order_type;not null;default:1"`
+	Amount    float64   `gorm:"column:amount;type:decimal(10,2);not null"`
+	Status    int       `gorm:"column:status;not null;default:1"`
+	CreatedAt time.Time `gorm:"column:created_at;type:datetime;default:CURRENT_TIMESTAMP;not null"`
 }
 
 const TableNameUserOrders = "user_orders"
